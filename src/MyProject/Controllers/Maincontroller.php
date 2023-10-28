143bc36e45e92d9a073b3f7ca11c066a46a3eda2
Filipp_Timos <filipptim000@gmail.com>
2023-10-29 02:21:59 +0300

    Реализована ORM

--- src/MyProject/Controllers/Maincontroller.php
+++ src/MyProject/Controllers/Maincontroller.php
@@ -1,33 +1,26 @@
 <?php
 
 namespace MyProject\Controllers;
+
+use MyProject\Models\Articles\Article;
 use MyProject\View\View;
 use MyProject\Services\Db;
 
 class MainController
 {
-    private $view;
-    private $db;
-
-    public function __construct()
-    {
-        $this->view = new View(__DIR__ . '/../../../templates');
-        $this->db = new Db();
-    }
-    public function main()
-    {
-        $articles = $this->db->query('SELECT * FROM `articles`;');
-        $this->view->renderHtml('main/main.php', ['articles' => $articles]);
-    }
-    public function sayHello(string $name)
-    {
-        $this->view->renderHtml('main/hello.php', ['name' => $name,'title'=> 'Страница приветсвия']);
+     private $view;
+     private $db;
  
-    }
-    public function sayBay(string $name)
-    {
-        $this->view->renderHtml('main/bay.php', ['name' => $name,'title'=> 'Страница прощания']);
-    }
+     public function __construct()
+     {
+         $this->view = new View(__DIR__ . '/../../../templates');
+         $this->db = new Db();
+     }
+ 
+     public function main()
+     {
+        $articles = $this->db->query('SELECT * FROM `articles`;', [], Article::class);
+        $this->view->renderHtml('main/main.php', ['articles' => $articles]);
+     }
 }
-
 ?>
