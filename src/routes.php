5ad0628d5292fc36cf9f621bd42ed784f7c9b036
Filipp_Timos <filipptim000@gmail.com>
2023-10-28 19:26:30 +0300

    Выполнено задание 3.1

--- src/routes.php
+++ src/routes.php
@@ -3,5 +3,6 @@ return [
     '~^hello/(.*)$~' => [\MyProject\Controllers\MainController::class, 'sayHello'],
     '~^bay/(.*)$~' => [\MyProject\Controllers\MainController::class, 'sayBay'],
     '~^$~' => [\MyProject\Controllers\MainController::class, 'main'],
+    '~^articles/(\d+)$~' => [\MyProject\Controllers\ArticlesController::class, 'view']
     ];
 ?>
